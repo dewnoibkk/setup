0a731aeebf953e1b2c48766f7eb065409e3ec6ce
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2018-10-22 20:31:05 -0700

    Initialize controller test

---
+++ Setup.Api.Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Setup.Api.Controllers;
+using Test.Core;
+
+namespace Setup.Api.Tests.Controllers
+{
+    public class ValuesControllerTests : BaseTestFixture
+    {
+        private ValuesController target;
+
+        public override void Setup()
+        {
+            base.Setup();
+            this.target = new ValuesController();
+        }
+
+        [Test]
+        public void Get_Success()
+        {
+            var result = this.target.Get();
+
+            Assert.AreEqual(2, result.Value.Count());
+        }
+    }
+}
---
+++ Test.Core/BaseTestFixture.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Test.Core
+{
+    [ExcludeFromCodeCoverage, TestFixture, Parallelizable(ParallelScope.All)]
+    public abstract class BaseTestFixture
+    {
+        /// <summary>
+        /// Identifies methods to be called once prior to any child tests.
+        /// </summary>
+        [OneTimeSetUp]
+        public virtual void Initialize() { }
+
+        /// <summary>
+        /// Indicates a method of a TestFixture called just before each test method.
+        /// </summary>
+        [SetUp]
+        public virtual void Setup() { }
+
+        /// <summary>
+        /// Marks a class with one-time setup or teardown methods for all the test fixtures in a namespace.
+        /// </summary>
+        [TearDown]
+        public virtual void TearDown() { }
+
+        /// <summary>
+        /// Identifies methods to be called once after all child tests.
+        /// </summary>
+        [OneTimeTearDown]
+        public virtual void Completed() { }
+    }
+}
